8db33b1807c4c967e82c72d2f0c9c78352b6fe5d
lw <105210815+1579704169@users.noreply.github.com>
2023-03-21 09:13:42 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import {config} from "./config.js";
 const chatGPTBot = new ChatGPTBot();
 
 const bot =  WechatyBuilder.build({
+  name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
   puppet: "wechaty-puppet-wechat",
   puppetOptions: {
     uos: true
   }
-  name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
 });
 async function main() {
   const initializedAt = Date.now()
